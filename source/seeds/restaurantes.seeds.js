67d249b0d4472eb5e8cbf44226e43aa7a89305e3
George <georgekutsia@gmail.com>
2023-08-17 09:32:21 +0100

    seeds

---
+++ source/seeds/restaurantes.seeds.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const Restaurante = require("../models/restaurantes.model");
+const restaurantesData = require("../dbs/restaurantes.db");
+
+const DB_URL = process.env.DB_URL;
+console.log(restaurantesData);
+mongoose
+  .connect(DB_URL)
+  .then(async()=> {
+    Restaurante.collection.drop();
+    const restaurantsArray = restaurantesData.map((rest) => new Restaurante(rest));
+    await Restaurante.insertMany(restaurantsArray);
+      console.log("restaurantes  creados correctamente")
+  })
+  .catch((error) => console.log("error insertando los Restaurantes", error))
+  .finally(() => mongoose.disconnect());

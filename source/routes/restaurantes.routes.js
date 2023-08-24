321dfc9a1f38b7bb6787d30b3b1190c1194fadf0
George <georgekutsia@gmail.com>
2023-08-24 03:20:19 +0100

    mejorando back

--- source/routes/restaurantes.routes.js
+++ source/routes/restaurantes.routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const {getRestaurante, postRestaurante, updateRestaurante, deleteRestaurante,getRestauranteById} = require("../controllers/restaurantes.controllers")
+const { isAuthenticated, isAdmin } = require("../middlewares/auth");
 
 const restRouter = express.Router();
 
-restRouter.get("/", getRestaurante);
+restRouter.get("/",  getRestaurante);
 restRouter.post("/", postRestaurante);
 restRouter.put("/:id", updateRestaurante);
 restRouter.delete("/:id", deleteRestaurante)
